f1acff5ea0cca08f74b03dce1734bc71a7fd9963
bakhbk <bakhbk@gmail.com>
2026-06-26 02:48:18 +0300

    fix(installers): add fzf bin to PATH in install script
    Signed-off-by: bakhbk <bakhbk@gmail.com>

--- installers/install_fzf.sh
+++ installers/install_fzf.sh
@@ -3,5 +3,6 @@
 if ! command -v fzf >/dev/null; then
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install --key-bindings --completion --update-rc
+  export PATH="$HOME/.fzf/bin:$PATH"
   source <(fzf --zsh)
 fi
